794334bd0d0d2f23e9fa2f21b7f2df71dd52a676
Lukas Bals <mail@lukasbals.at>
2023-05-26 15:12:48 +0200

    Use SSL mode on production

--- api/sequelize.ts
+++ api/sequelize.ts
@@ -5,7 +5,17 @@ let sequelize: Sequelize = null;
 const getSequelize = (): Sequelize => {
   if (sequelize) return sequelize;
 
-  sequelize = new Sequelize(process.env.POSTGRES_URL, { ssl: true });
+  sequelize = new Sequelize(process.env.POSTGRES_URL, {
+    dialectOptions:
+      process.env.NODE_ENV === 'production'
+        ? {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false,
+            },
+          }
+        : {},
+  });
   return sequelize;
 };
 
